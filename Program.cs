507aa90f0032ae9b37c66b4c4a54efcffc26493a
Ktanho <Ktanho@gmail.com>
2023-03-09 11:51:45 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Ejercicio 1
+
+Console.WriteLine("Introduce tu Nombre");
+String nombre = Console.ReadLine();
+Console.WriteLine("Hola, mi nombre es: " + nombre);
+
+// Ejercicio 2
+
+Console.WriteLine("Introducir Hora");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora es " + hora);    
